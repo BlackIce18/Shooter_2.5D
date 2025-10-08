219214e218f7312643faa161dc157a1ef12e7407
BlackIce18 <sdsdfhshs.dima@yandex.ru>
2025-10-08 14:08:19 +0300

    init

---
+++ Assets/Game/Scripts/CameraMovement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private Camera _mainCamera;
+    [SerializeField] private GameObject _lookTarget;
+    [SerializeField] private float _smoothDelay;
+    
+    private Vector3 _offset;
+    private void Start()
+    {
+        if(_mainCamera == null) _mainCamera = Camera.main;
+
+        _offset = _mainCamera.transform.position - _lookTarget.transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 targetCamPos = _lookTarget.transform.position + _offset;
+        _mainCamera.transform.position = Vector3.Lerp (_mainCamera.transform.position, targetCamPos, _smoothDelay * Time.deltaTime);
+    }
+}
---
+++ Assets/Game/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private GameObject _player;
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private PlayerInput _playerInput;
+    [SerializeField] private Animator _animator;
+    
+    private void Update()
+    {
+        Vector2 inputDirection = _playerInput.actions["Move"].ReadValue<Vector2>();
+        if (inputDirection.Equals(Vector2.zero))
+        {
+            _animator.SetBool("IsWalking", false);
+        }
+        else
+        {
+            _animator.SetBool("IsWalking", true);
+        }
+        Debug.Log(inputDirection);
+        switch (inputDirection)
+        {
+            case Vector2 v when v.Equals(Vector2.up):
+                _animator.SetFloat("X", 0f);
+                _animator.SetFloat("Y", 1f);
+                break;
+            case Vector2 v when v.Equals(Vector2.right):
+                _animator.SetFloat("X", 1f);
+                _animator.SetFloat("Y", 0f);
+                break;
+            case Vector2 v when v.Equals(Vector2.down):
+                _animator.SetFloat("X", 0f);
+                _animator.SetFloat("Y", -1f);
+                break;
+            case Vector2 v when v.Equals(Vector2.left):
+                _animator.SetFloat("X", -1f);
+                _animator.SetFloat("Y", 0f);
+                break;
+        }
+        _player.transform.Translate(new Vector3(inputDirection.x, 0, inputDirection.y) * _moveSpeed * Time.deltaTime, Space.World); 
+    }
+}
